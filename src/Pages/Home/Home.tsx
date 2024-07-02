3f586cfc4d8533b92f76d430a24235e25cdf1b31
Atik Sahariyar <atiksahariyar88@gmail.com>
2024-07-02 20:19:57 +0600

    initial project setup

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            <h3>Home Page</h3>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Home;
